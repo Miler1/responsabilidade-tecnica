dac0ded806093b2512b4d34852d99dac60e934a8
raydson.ferreira <raydson.ferreira.lemaf@gmail.com>
2020-11-27 12:20:11 -0300

    Commit inicial do backend

---
+++ backend/src/main/java/com/gestaoresponsabilidadetecnica/GestaoresponsabilidadetecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.gestaoresponsabilidadetecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoresponsabilidadetecnicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoresponsabilidadetecnicaApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/gestaoresponsabilidadetecnica/GestaoresponsabilidadetecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaoresponsabilidadetecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoresponsabilidadetecnicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
