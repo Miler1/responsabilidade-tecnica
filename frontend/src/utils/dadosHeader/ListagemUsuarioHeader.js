7667caa45a82e4eae15014c8a026b2955f2010fe
Leonardo Maranha de Oliveira <leonardo.maranha.lemaf@gmail.com>
2020-12-09 12:34:03 -0200

    Altera e padroniza label dos headers de usuário e administrador

--- frontend/src/utils/dadosHeader/ListagemUsuarioHeader.js
+++ frontend/src/utils/dadosHeader/ListagemUsuarioHeader.js
@@ -1,4 +1,5 @@
 const HEADER = [
+	{ text: 'Nome', value: 'responsavel.nome'},
 	{ text: 'Status', value: 'status.nome', width: '155px'},
 	{ text: 'Validade', value: 'validade'},
 	{ text: 'Ações', value: 'actions', sortable: false, align: 'center', width: '100px' },
