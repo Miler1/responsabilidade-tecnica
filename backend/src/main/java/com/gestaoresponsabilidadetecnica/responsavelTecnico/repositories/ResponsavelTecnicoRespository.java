a7e48f33150ad9100091a82633b324757c97ad16
kellyson.santos <kellyson.santos.lemaf@gmail.com>
2020-12-14 15:49:55 -0300

    10 - Job de verificação de responsáveis vencidos

--- backend/src/main/java/com/gestaoresponsabilidadetecnica/responsavelTecnico/repositories/ResponsavelTecnicoRespository.java
+++ backend/src/main/java/com/gestaoresponsabilidadetecnica/responsavelTecnico/repositories/ResponsavelTecnicoRespository.java
@@ -2,6 +2,9 @@ package com.gestaoresponsabilidadetecnica.responsavelTecnico.repositories;
 
 import com.gestaoresponsabilidadetecnica.responsavelTecnico.models.ResponsavelTecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
-public interface ResponsavelTecnicoRespository extends JpaRepository<ResponsavelTecnico, Integer> {
+@Repository
+public interface ResponsavelTecnicoRespository extends JpaRepository<ResponsavelTecnico, Integer>, JpaSpecificationExecutor<ResponsavelTecnico> {
 }
