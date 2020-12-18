43589735d196824a2c89eeee19c1f4e2704bc24f
miler.boell <milerboell.zetta@ufla.br>
2020-12-18 16:51:36 -0300

    Remoção da função de conversão de tipo Blob para File não utilizada

--- frontend/src/views/usuario/cadastro/Cadastro.vue
+++ frontend/src/views/usuario/cadastro/Cadastro.vue
@@ -688,13 +688,6 @@ export default {
 
 		},
 
-		blobToFile(theBlob, fileName){
-			//A Blob() is almost a File() - it's just missing the two properties below which we will add
-			theBlob.lastModifiedDate = new Date();
-			theBlob.name = fileName;
-			return theBlob;
-		}
-
 	},
 
 	mounted() {
